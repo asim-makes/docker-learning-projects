260ae0de1d98915460fa4b0da2d6dc8b05d67443
asim-makes <asim.baralapl@gmail.com>
2025-11-19 16:27:31 +0545

    Project 2: Multi-stage build. Remaining: Monitoring

---
+++ 02-multi-stage-build-with-custom-monitoring/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import time
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+    'app_http_requests_total',
+    'Total number of HTTP requests to the application',
+    ['method', 'endpoint']
+)
+
+
+@app.route("/")
+def home():
+    time.sleep(0.05)
+
+    REQUEST_COUNT.labels(method='GET', endpoint='/').inc()
+
+    return "Hello from the Optimized Microservice!", 200
+
+@app.route("/metrics")
+def metrics():
+    REQUEST_COUNT.labels(method='GET', endpoint='/metrics').inc()
+
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
